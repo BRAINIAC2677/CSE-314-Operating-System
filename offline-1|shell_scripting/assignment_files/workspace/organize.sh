9105249e13d680902d9b4b8de2145065dcd57763
BRAINIAC2677 <brainiac2677@gmail.com>
2023-06-19 23:11:40 +0600

    same output but optimized code

--- offline-1|shell_scripting/assignment_files/workspace/organize.sh
+++ offline-1|shell_scripting/assignment_files/workspace/organize.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-get_file_name()
-{
-    dir=$1
-    IFS="/"
-    splitted_components=($dir)
-    unset IFS
-    echo ${splitted_components[-1]}
-}
-
 get_id()
 {
     file_name=${1%????}
@@ -20,246 +11,112 @@ get_id()
 
 get_extension()
 {
-    file_name=$1
-    IFS='.'
-    splitted_components=($file_name)
-    unset IFS 
-    echo ${splitted_components[-1]}
+    filename=`basename "${1}"`
+    echo ${filename##*.}
 }
 
-# return value 0->not source code, 1->c file, 2->python file, 3->java file
-get_extension_code()
-{
-    file_name="$1"
-    file_extension=`get_extension "$file_name"`
-    if [ "$file_extension" == "c" ]
-    then 
-        echo "1"
-    elif [ "$file_extension" == "py" ]
-    then 
-        echo "2"
-    elif [ "$file_extension" == "java" ]
-    then 
-        echo "3" 
-    else 
-        echo "0"
-    fi 
-}
-
-get_source_code_file()
+get_source_code_path()
 {
 	if [ -d "$1" ]
 	then
-	
 		for i in "$1"/*
 		do
-			get_source_code_file "$i"
+			get_source_code_path "$i"
 		done
-	
 	elif [ -f "$1" ]
 	then
-        extension_code=`get_extension_code "$1"`
-        if [ "$extension_code" != "0" ]
+        extension=`get_extension "$1"`
+        if [ "$extension" == "c" ] || [ "$extension" == "py" ] || [ "$extension" == "java" ]
         then 
             echo "$1"
         fi 
 	fi
 }
 
-generate_output_c()
+generate_and_match_output()
 {
-    c_file="$1"
-    test_dir="$2"
-    gcc "$c_file" -o "main.out"
+    id="$1"
+    extension="$2"
+    id_root_dir="${targets_folder_name}/${file_type[$extension]}/${id}"
+    cd $id_root_dir
+    ${compilation_commands[$extension]}
     counter=1
-    for test in "$test_dir"/*
-    do
-        ./main.out < "$test" > "out${counter}.txt"
-        counter=$( expr $counter + 1 )
-    done
-}
-
-generate_output_py()
-{
-    py_file="$1"
-    test_dir="$2"
-    counter=1
-    for test in "$test_dir"/*
-    do
-        python3 "$py_file" < "$test" > "out${counter}.txt"
-        counter=$( expr $counter + 1 )
-    done
-}
-
-generate_output_java()
-{
-    java_file="$1"
-    test_dir="$2"
-    javac "$java_file"
-    counter=1
-    for test in "$test_dir"/*
-    do
-        java Main < "$test" > "out${counter}.txt"
-        counter=$( expr $counter + 1 )
-    done
-}
-
-count_matched_unmatched()
-{
-    answer_dir=$1
     matched=0
-    unmatched=0
-    counter=1
-    while [ true ]
-    do  
+    for test in "../../../${tests_folder_name}"/*
+    do
         current_out_file="out${counter}.txt"
-        current_answer_file="${answer_dir}/ans${counter}.txt"
-        if [ ! -e "$current_out_file" ] || [ ! -e "$current_answer_file" ]
-        then 
-            break
-        fi
-        differences=`diff "$current_out_file" "$current_answer_file"`
+        current_ans_file="../../../${ans_folder_name}/ans${counter}.txt"
+        ${run_commands[$extension]} < "$test" > "${current_out_file}"
+        differences=`diff "${current_out_file}" "${current_ans_file}"`
         if [ "${#differences}" == "0" ]
         then 
             matched=$(( $matched + 1 ))
-        else 
-            unmatched=$(( $unmatched + 1 ))
-        fi
-        counter=$(( $counter + 1 ))
+        fi 
+        counter=$( expr $counter + 1 )
     done
-    echo "$matched $unmatched"
+    cd ../../..
+    echo "${id},${file_type[$extension]},${matched},$(( ${counter} - 1 - ${matched} ))" >> "${targets_folder_name}/result.csv" 
 }
 
-# assumes currently in id folder
-add_csv_entry()
+organize_file()
 {
-    id=$1
-    type=$2
-    csv_file_path="$3"
-    answer_dir="../../../${answers_folder_name}"
-    count_arr=(`count_matched_unmatched "$answer_dir"`)
-    matched=${count_arr[0]}
-    unmatched=${count_arr[1]}
-    echo "${id},${type},${matched},${unmatched}" >> "$csv_file_path" 
-}
-
-organize_and_execute()
-{
-    id=$1
-    source_code_file=$2
-    cd "$targets_folder_name"
-    extension_code=`get_extension_code "$source_code_file"`
-
+    file_path=$1
+    file_name=`basename "$file_path"`
+    id=`get_id "$file_name"`
+    mkdir "$id" 
+    cp "${submissions_folder_name}/${file_name}" "$id"
+    unzip -qq "${id}/${file_name}" -d "${id}"
+    source_code_path=`get_source_code_path "$id"`
+    extension=`get_extension "$source_code_path"`
     if [  "$verbose" == "true" ]
     then 
         echo "organizing files of $id"
     fi
-
-    if [ "$extension_code" == "1" ]
-    then 
-        cd C 
-        mkdir "$id"
-        cd "$id"
-        cp "../../../${submissions_folder_name}/$source_code_file" main.c 
-        test_dir="../../../${tests_folder_name}"
-        if [ "$noexecute" == "false" ]
-        then
-            if [ "$verbose" == "true" ]
-            then 
-                echo "Executing files of $id"
-            fi
-            generate_output_c "main.c" "$test_dir"
-            add_csv_entry "$id" "C" "../../../${targets_folder_name}/result.csv"
-        fi
-        cd ../..
-    elif [ "$extension_code" == "2" ]
-    then 
-        cd Python 
-        mkdir "$id"
-        cd "$id"
-        cp "../../../${submissions_folder_name}/$source_code_file" main.py 
-        test_dir="../../../${tests_folder_name}"
-        if [ "$noexecute" == "false" ]
-        then
-            if [ "$verbose" == "true" ]
-            then 
-                echo "Executing files of $id"
-            fi
-            generate_output_py "main.py" "$test_dir"
-            add_csv_entry "$id" "Python" "../../../${targets_folder_name}/result.csv"
-        fi
-        cd ../..
-    elif [ "$extension_code" == "3" ]
-    then 
-        cd Java 
-        mkdir "$id"
-        cd "$id"
-        cp "../../../${submissions_folder_name}/$source_code_file" Main.java 
-        test_dir="../../../${tests_folder_name}"
-        if [ "$noexecute" == "false" ]
-        then
-            if [ "$verbose" == "true" ]
-            then 
-                echo "Executing files of $id"
-            fi
-            generate_output_java "Main.java" "$test_dir"
-            add_csv_entry "$id" "Java" "../../../${targets_folder_name}/result.csv"
-        fi
-        cd ../..
-    fi 
-    cd ..
+    id_root_dir="${targets_folder_name}/${file_type[$extension]}/${id}"
+    mkdir ${id_root_dir}
+    cp "${source_code_path}" "${id_root_dir}/${desired_filename[$extension]}"
+    rm -r "$id"
 }
 
 create_targets_folder()
 {
-    # create targets folder if doesn't exists
-    if ! [ -a "$targets_folder_name" ]
+    if [ -a "$targets_folder_name" ]
     then 
-        mkdir "$targets_folder_name"
-        cd "$targets_folder_name"
-        mkdir C Python Java
-        if [ "$noexecute" == "false" ]
-        then
-            touch result.csv
-            echo "student_id,type,matched,not_matched" >> result.csv
-        fi
-        cd ..
+        rm -r "$targets_folder_name"
     fi
+    mkdir "$targets_folder_name"
+    cd "$targets_folder_name"
+    mkdir C Python Java
+    if [ "$noexecute" == "false" ]
+    then
+        touch result.csv
+        echo "student_id,type,matched,not_matched" >> result.csv
+    fi
+    cd ..
 }
 
 main() 
 {
     create_targets_folder
-    for file_name in "$submissions_folder_name"/*
+    for file_path in "$submissions_folder_name"/*
     do
-        file_name=`get_file_name "$file_name"`
-        id=`get_id "$file_name"`
-        cd "$submissions_folder_name"
-        mkdir "$id" 
-        cp "$file_name" "$id"
-        cd "$id" 
-        unzip -qq "$file_name"
-        cd ..
-        source_code_file=`get_source_code_file "$id"`
-        cd ..
-        organize_and_execute "$id" "$source_code_file"
-        cd "$submissions_folder_name"
-        rm -r "$id"
-        cd ..
+        organize_file "$file_path"
+        if [ "$noexecute" == "false" ]
+        then
+            if [ "$verbose" == "true" ]
+            then 
+                echo "Executing files of $id"
+            fi
+            generate_and_match_output "$id" "$extension" 
+        fi
     done
 }
-
-
-submissions_folder_name=$1
-targets_folder_name=$2
-tests_folder_name=$3
-answers_folder_name=$4
-verbose=false
-noexecute=false
 
 if [ $# -lt 4 ]
 then 
     echo -e "Missing arguments.\n
+man page
+---------\n
     NAME
         organize - organize, execute, generate output from test files and match output against answers for students submissions.\n
     SYNOPSIS
@@ -283,6 +140,21 @@ then
         "
     exit 
 fi
+
+submissions_folder_name=$1
+targets_folder_name=$2
+tests_folder_name=$3
+ans_folder_name=$4
+verbose=false
+noexecute=false
+id=""
+extension=""
+
+declare -A compilation_commands=( [c]="gcc main.c -o main.out" [py]="" [java]="javac Main.java" )
+declare -A run_commands=( [c]="./main.out" [py]="python3 main.py" [java]="java Main" )
+declare -A file_type=( [c]="C" [py]="Python" [java]="Java" )
+declare -A desired_filename=( [c]="main.c" [py]="main.py" [java]="Main.java" )
+
 if [ $# -gt 4 ] && [ "$5" == "-v" ]
 then
     verbose=true
